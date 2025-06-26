c6c2099f052b53f44d539b36f53d0ce06f38793e
joejohn98 <joejohn048@gmail.com>
2025-06-26 23:38:14 +0530

    implement weather data fetching in WeatherTemperature component

--- src/WeatherTemperature.tsx
+++ src/WeatherTemperature.tsx
@@ -1,11 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { dummyFetch, type WeatherData } from "./data/dummyfetch";
 
 const WeatherTemperature = () => {
-    const [weatherData, setWeatherData] = useState([]);
+    const [weatherData, setWeatherData] = useState({} as WeatherData);
     const [isCelsius, setIsCelsius] = useState(true);
     const [loading, setLoading] = useState(false);
 
-    
+    useEffect(() => {
+        const fetchWeather = async() => {
+            setLoading(true)
+            try {
+                const response = await dummyFetch("https://example.com/api/weather")
+                if(response.status === 200){
+                    setWeatherData(response.data)
+                }
+            } catch (error) {
+                if (error instanceof Error) {
+                    console.log("Error fetching data", error.message);
+                } else {
+                    console.log("Error fetching data", error);
+                }
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchWeather()
+    }, [])
+
   return (
     <div>
         <h2>Weather Temperature</h2>
